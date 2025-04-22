a17ee3342185d81d2f5e85e4a883fe09aec37043
Olga Chrostowska <olga.chrostowska@siili.com>
2025-04-22 18:05:05 +0200

    Wrap products sectopns in columns

--- components/ProductsList/ProductsList.tsx
+++ components/ProductsList/ProductsList.tsx
@@ -1,4 +1,5 @@
 import { ProductTypeBasic } from "@/data/types";
+import styled from "styled-components";
 import { ProductSectionBasic } from "./ProductSectionBasic";
 
 type Props = { productTypes: ProductTypeBasic[] };
@@ -6,12 +7,37 @@ export const ProductsList = ({ productTypes }: Props) => {
   if (!productTypes?.length) return null;
 
   return (
-    <>
+    <ProductItems>
       {productTypes.map((section, index) => {
         const { label } = section;
 
         return <ProductSectionBasic key={label} {...section} />;
       })}
-    </>
+    </ProductItems>
   );
 };
+
+const ProductItems = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  row-gap: 3rem;
+
+  & > * {
+    flex: 1 1 calc(33.333% - 2rem);
+    max-width: calc(33.333% - 2rem);
+  }
+
+  @media ${(props) => props.theme.media.tabPort} {
+    flex-direction: column;
+    flex-wrap: nowrap;
+    gap: 0rem;
+    margin-top: 0rem;
+    margin-bottom: 0rem;
+
+    & > * {
+      flex: 1 1 100%;
+      max-width: 100%;
+    }
+  }
+`;
